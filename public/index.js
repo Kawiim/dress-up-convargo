d7270891ac96c5ed26de70422dc4480ac6de4ae3
Karim Baali <baali.karim95@gmail.com>
2018-01-27 16:47:20 +0100

    Improving results display for user

--- public/index.js
+++ public/index.js
@@ -6,12 +6,13 @@
     const fragment = document.createDocumentFragment();
     const div = document.createElement('div');
     const template = actors.map(actor => {
+      var type;
+      actor.type == "credit" ? type = 'success' : type = 'danger';
       return `
-        <div class="actor">
+        <li class="list-group-item d-flex justify-content-between align-items-center">
           <span>${actor.who}</span>
-          <span>${actor.type}</span>
-          <span>${actor.amount}</span>
-        </div>
+          <span class="label label-${type} label-pill">${actor.amount}</span>
+        </li>
       `;
     }).join('');
 
@@ -19,6 +20,8 @@
     fragment.appendChild(div);
     document.querySelector('#actors').innerHTML = '';
     document.querySelector('#actors').appendChild(fragment);
+    document.querySelector('#helper').classList.add('visible')
+    document.querySelector('#helper').classList.remove('invisible');
   };
 
   const button = document.querySelector('#compute');
